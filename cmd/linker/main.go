593958442554c004deab5ca1de2efb35b391eb3e
Hyojun Jeon <gywns2702@gmail.com>
2020-12-20 05:53:32 +0900

    Fix gitignore to include /cmd and Add missing cmd directory

---
+++ cmd/linker/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/devinjeon/linker/internal/handlers/auth"
+	"github.com/devinjeon/linker/internal/handlers/links"
+	m "github.com/devinjeon/linker/internal/middleware"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+// This Lambda function is triggered by API Gateway
+// so it should use Request and Response types from events.APIGatewayProxy*.
+
+type (
+	response = m.Response
+	request  = m.Request
+	// Handler is type of handler function
+	Handler func(request) (response, error)
+)
+
+type router struct {
+	handlers map[string]Handler
+}
+
+func newRouter() router {
+	r := router{
+		handlers: make(map[string]Handler),
+	}
+	return r
+}
+
+func (r *router) addHandler(path string, handler Handler) {
+	if path == "" {
+		path = "/"
+	}
+
+	if path[0] != '/' {
+		path = "/" + path
+	}
+
+	if path[len(path)-1] == '/' {
+		path = path[:len(path)-1]
+	}
+
+	r.handlers[path] = handler
+}
+
+func (r *router) route(orgReq events.APIGatewayProxyRequest) (response, error) {
+	req := m.WrapAPIGatewayProxyRequest(orgReq)
+
+	badRequest := response{StatusCode: 400}
+	path := req.Path
+	if path == "" {
+		path = "/"
+	}
+	if path[0] != '/' {
+		path = "/" + path
+	}
+
+	for p, h := range r.handlers {
+		if !strings.HasPrefix(path, p) {
+			continue
+		}
+
+		// NOTE: request path should not match the case
+		//       that handler path is "/{req.path}xxxx/yyyy"
+		if path[len(path)-1] != '/' {
+			path = path + "/"
+		}
+		if strings.HasPrefix(path, p+"/") {
+			// NOTE: handler gets only subpath
+			subpath := strings.TrimPrefix(path, p)
+			subpath = strings.TrimSuffix(subpath, "/")
+			req.Path = subpath
+
+			return h(req)
+		}
+	}
+
+	return badRequest, nil
+}
+
+func main() {
+	r := newRouter()
+
+	r.addHandler("/links", links.Handler)
+	r.addHandler("/auth", auth.Handler)
+
+	lambda.Start(r.route)
+}
