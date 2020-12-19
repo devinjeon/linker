b57fa160d8f8e47d77c981981f48e1489be53f8e
Hyojun Jeon <gywns2702@gmail.com>
2020-12-20 05:26:35 +0900

    Implement middleware.ClearCookie

--- internal/middleware/cookie.go
+++ internal/middleware/cookie.go
@@ -8,6 +8,8 @@ import (
 
 var domain = os.Getenv("LINKER_DOMAIN")
 
+const maxAge int = 3600 * 24 * 90
+
 func parseCookie(cookies string) map[string]string {
 	cookiesMap := make(map[string]string)
 	for _, c := range strings.Split(cookies, ";") {
@@ -32,6 +34,7 @@ func SetCookie(name string, value string, resp *Response) {
 	cookie = cookie + "; Secure"
 	cookie = fmt.Sprintf("%s; Domain=%s", cookie, domain)
 	cookie = cookie + "; Path=/"
+	cookie = fmt.Sprintf("%s; Max-Age=%d", cookie, maxAge)
 
 	var cookies []string
 	headers := resp.MultiValueHeaders
@@ -44,4 +47,31 @@ func SetCookie(name string, value string, resp *Response) {
 		cookies = []string{}
 	}
 	resp.MultiValueHeaders["Set-Cookie"] = append(cookies, cookie)
+}
+
+// ClearCookie removes all cookies
+func ClearCookie(req Request, resp *Response) {
+	var cookies []string
+	headers := resp.MultiValueHeaders
+	if headers == nil {
+		resp.MultiValueHeaders = make(map[string][]string)
+	}
+
+	cookies, ok := resp.MultiValueHeaders["Set-Cookie"]
+	if !ok {
+		cookies = []string{}
+	}
+
+	for name := range req.Cookies {
+		cookie := fmt.Sprintf("%s=%s", name, "")
+		cookie = cookie + "; SameSite=Lax"
+		cookie = cookie + "; HttpOnly"
+		cookie = cookie + "; Secure"
+		cookie = fmt.Sprintf("%s; Domain=%s", cookie, domain)
+		cookie = cookie + "; Path=/"
+		cookie = cookie + "; Max-Age=0"
+		cookies = append(cookies, cookie)
+	}
+
+	resp.MultiValueHeaders["Set-Cookie"] = cookies
 }
