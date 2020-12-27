e02f432aff4b2477fd42ccac8253fe602af17eff
Hyojun Jeon <gywns2702@gmail.com>
2020-12-28 03:07:26 +0900

    Create test UI

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,78 @@
 import logo from './logo.svg';
 import './App.css';
+import { useCookies } from 'react-cookie';
+import { Component } from 'react';
+import axios from 'axios';
+axios.defaults.baseURL = 'https://api.linker.hyojun.me';
 
 function App() {
+  const [cookies] = useCookies(["session_id"]);
+
+  function hasSession() {
+    return cookies.session_id !== undefined;
+  }
+
+  function createNewLink(data) {
+    console.log(data);
+    return axios.post(`/links/${data.id}`, {"url": data.url}, {withCredentials: true})
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LinkFrom onCreate={createNewLink} />
+        <SignButton hasSession={hasSession()} />
       </header>
     </div>
   );
+}
+
+function SignButton(props) {
+  const hasSession = props.hasSession;
+  if (!hasSession) {
+    return <a href="https://api.linker.hyojun.me/auth/signin">로그인</a>
+  } else {
+    return <a href="https://api.linker.hyojun.me/auth/signin">로그아웃</a>
+  }
+}
+
+class LinkFrom extends Component {
+  state = {
+    id: '',
+    url: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onCreate(this.state)
+      .then(response => {
+        if (this.response.status === 200) {
+          this.setState({
+            id: '',
+            url: ''
+          })
+        }
+      })
+      .catch(error => {
+        console.log(this.error);
+      });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input placeholder="ID" value={this.state.id} onChange={this.handleChange} name="id" />
+        <input placeholder="URL" value={this.state.url} onChange={this.handleChange} name="url" />
+        <input type="submit" value="등록" />
+      </form>
+    )
+  }
 }
 
 export default App;
