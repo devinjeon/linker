625835b001290fc05d01594a8916bbc8122ad81e
Hyojun Jeon <gywns2702@gmail.com>
2020-12-28 03:06:05 +0900

    Set CORS

--- cmd/linker/main.go
+++ cmd/linker/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"os"
+	"time"
 
 	"github.com/devinjeon/linker/internal/handlers/auth"
 	"github.com/devinjeon/linker/internal/handlers/links"
@@ -10,13 +12,24 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/awslabs/aws-lambda-go-api-proxy/gin"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
+var linkerDomain = os.Getenv("LINKER_DOMAIN")
 var ginLambda *ginadapter.GinLambda
 
 func init() {
 	r := gin.Default()
+
+	// CORS
+	config := cors.DefaultConfig()
+	config.AllowCredentials = true
+	config.AllowOrigins = []string{linkerDomain}
+	config.MaxAge = time.Hour * 24 * 30
+	r.Use(cors.New(config))
+
+	// Route
 	linksGroup := r.Group("/links")
 	{
 		linksGroup.GET("/:id", links.Redirect)
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/aws/aws-lambda-go v1.20.0
 	github.com/aws/aws-sdk-go v1.36.12
 	github.com/awslabs/aws-lambda-go-api-proxy v0.9.0
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
 	google.golang.org/protobuf v1.25.0
 )
